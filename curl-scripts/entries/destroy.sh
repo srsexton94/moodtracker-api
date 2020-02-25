44570520a5b6ee41edd1715d9be28aae48909fb8
srsexton94 <srsexton94@gmail.com>
2020-02-25 11:41:47 -0500

    Deletes curl scripts for never-made resources
    Deletes the curl scripts previously drafted for 'needs' and 'entries'
    resources - which were never implemented (possible stretch goal, though
    likely involved restructuring)

--- curl-scripts/entries/destroy.sh
+++
@@ -1,8 +0,0 @@
-# Ex: ID=idgoeshere TOKEN=tokengoeshere sh curl-scripts/examples/destroy.sh
-
-curl "http://localhost:4741/entries/${ID}" \
-  --include \
-  --request DELETE \
-  --header "Authorization: Token token=${TOKEN}" \
-
-echo
